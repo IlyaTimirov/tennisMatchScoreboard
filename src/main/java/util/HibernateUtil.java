9283d66beccbad06de46bd769dd7202d6ba50fab
IlyaTimirov <ilya.ekb.99@mail.ru>
2024-04-06 16:42:22 +0600

    add method getSession

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -1,30 +1,49 @@
 package util;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.util.function.Function;
+
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
     private static StandardServiceRegistry standardServiceRegistry;
 
-    public static SessionFactory getSessionFactory(){
-        if(sessionFactory == null){
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
             try {
                 standardServiceRegistry = new StandardServiceRegistryBuilder().configure().build();
                 MetadataSources sources = new MetadataSources(standardServiceRegistry);
                 Metadata metadata = sources.getMetadataBuilder().build();
                 sessionFactory = metadata.getSessionFactoryBuilder().build();
                 System.out.println("ok");
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
-                if(standardServiceRegistry != null){
+                if (standardServiceRegistry != null) {
                     StandardServiceRegistryBuilder.destroy(standardServiceRegistry);
                 }
             }
         }
         return sessionFactory;
+    }
+
+    public <T> T getSession(Function<Session, T> sessions) {
+        Transaction transaction = null;
+        try (Session session = getSessionFactory().openSession();) {
+            transaction = session.beginTransaction();
+            T result = sessions.apply(session);
+            transaction.commit();
+            return result;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(e);
+        }
     }
 }
